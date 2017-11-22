4f42e6beadf0da4c04ab2f33af656aa8ffae31e9
viery365 <20521892+viery365@users.noreply.github.com>
2017-11-22 19:55:38 +0000

    Comment about the functions used to get the uri

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ try {
 
   include __DIR__ . '/../includes/autoload.php';
 
+//this is still the best way. If the uri ends in '?' the route will not reject it - it just cuts the '?' off
+//(unlikely using either trim($string, '/') for both sides of the string or ltrim($string, '/') only);
   $route = ltrim(strtok($_SERVER['REQUEST_URI'], '?'), '/');
 
   $entryPoint = new \Ninja\EntryPoint($route, $_SERVER['REQUEST_METHOD'], new \Ijdb\IjdbRoutes());
